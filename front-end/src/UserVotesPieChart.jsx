bcfcd4fc0ed767532342d3c1291fcc2f96c9a53e
ZPanic0 <14856717+ZPanic0@users.noreply.github.com>
2020-04-12 17:44:07 -0400

    Optimize out a loop from layer generation logic

--- front-end/src/UserVotesPieChart.jsx
+++ front-end/src/UserVotesPieChart.jsx
@@ -3,60 +3,94 @@ import { PieChart, Pie, Cell, Tooltip } from 'recharts'
 import CustomTooltip from './CustomTooltip'
 
 export default class UserVotesPieChart extends Component {
-    max = 0
-
-    buildLayer(key, data, x, y, innerRadius, outerRadius) {
-        return <Pie
-            key={key}
-            dataKey="value"
-            data={data}
-            cx={x}
-            cy={y}
-            innerRadius={innerRadius}
-            outerRadius={outerRadius}>
-            {data.map(entry => <Cell
-                key={entry.name}
-                fill={this.props.colors[this.getColor(entry.heat)]}
-            />)}
-        </Pie>
+    state = {
+        layers: []
     }
 
-    getColor(input) {
-        if (input === 0) return 0
-        if (input === 1) return 1
+    constructor(props) {
+        super(props)
 
-        if (input / this.max <= 0.20) return 4
-        if (input / this.max <= 0.40) return 5
-        if (input / this.max <= 0.60) return 6
-        if (input / this.max <= 0.80) return 7
-        return 8
+        this.buildLayers = this.buildLayers.bind(this)
+    }
+
+    componentWillMount() {
+        this.setState({ layers: this.buildLayers() })
+    }
+
+    buildLayers() {
+        let layerData = [] //both?
+        let cellData = [] //maybe declare the size of this?
+        let newLayers = []
+        let innerRadius = 60
+        let outerRadius = 80
+        let previousLayerIndex = 0
+        let layerIndex
+        for (let [key, value] of Object.entries(this.props.rawData)) {
+            layerIndex = (key - 1) / 100 << 0
+
+            //If layer is fully represented in cellData, roll up layer
+            if (previousLayerIndex < layerIndex) {
+
+                newLayers[layerIndex] = <Pie
+                    key={`layer${layerIndex}`}
+                    dataKey="value"
+                    data={layerData}
+                    cx={this.props.canvasWidth / 2}
+                    cy={this.props.canvasHeight / 2}
+                    innerRadius={innerRadius}
+                    outerRadius={outerRadius}
+                >
+                    {cellData}
+                </Pie>
+
+                //Clear layerData and cellData
+                layerData = []
+                cellData = []
+
+                //Update radii for next layer
+                innerRadius += 30
+                outerRadius += 30
+
+                //Update previous layerIndex
+                previousLayerIndex = layerIndex
+            }
+
+            layerData[(key - 1) % 100] = { name: key, value: 1, heat: value.length, who: value }
+            cellData[(key - 1) % 100] = <Cell
+                key={key}
+                fill={this.props.colors[((input, max) => {
+                    if (input === 0) return 0
+                    if (input === 1) return 1
+
+                    if (input / max <= 0.20) return 4
+                    if (input / max <= 0.40) return 5
+                    if (input / max <= 0.60) return 6
+                    if (input / max <= 0.80) return 7
+                    return 8
+                })(value.length, 20)]}
+            />
+        }
+
+        layerIndex++
+        newLayers[layerIndex] = <Pie
+            key={`layer${layerIndex}`}
+            dataKey="value"
+            data={layerData}
+            cx={this.props.canvasWidth / 2}
+            cy={this.props.canvasHeight / 2}
+            innerRadius={innerRadius}
+            outerRadius={outerRadius}
+        >
+            {cellData}
+        </Pie>
+
+        return newLayers
     }
 
     render() {
-        this.max = 0
-
-        const myData = Object.entries(this.props.rawData).map(([key, value]) => {
-            this.max = value.length > this.max ? value.length : this.max
-
-            return ({ name: key, value: 1, heat: value.length, who: value })
-        })
-
-        let layers = []
-
-        for (let i = 0, startRadius = 60, endRadius = 80; i < 10; i++, startRadius += 30, endRadius += 30) {
-            layers[i] = this.buildLayer(
-                `layer${i}`,
-                myData.slice(i * 100 + 1, i * 100 + 99),
-                this.props.canvasWidth / 2,
-                this.props.canvasHeight / 2,
-                startRadius,
-                endRadius
-            )
-        }
-
         return (
             <PieChart width={this.props.canvasWidth} height={this.props.canvasHeight}>
-                {layers}
+                {this.state.layers}
                 <Tooltip
                     contentStyle={{
                         backgroundColor: 'rgba(255,255,255,0.75)',
