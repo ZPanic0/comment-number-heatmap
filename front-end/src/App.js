1cab3b4d0188d4f1d0101c50f58510f571df0e0d
ZPanic0 <14856717+ZPanic0@users.noreply.github.com>
2020-04-12 13:56:37 -0400

    Move some static values to configurable props

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -12,11 +12,16 @@ export default class App extends Component {
   state = {
     rawData: null
   }
-  
+
   render() {
     return (
       <div className="App">
-        {this.state.rawData && <UserVotesPieChart rawData={this.state.rawData} />}
+        {this.state.rawData && <UserVotesPieChart
+          rawData={this.state.rawData}
+          colors={['#ffffe5', '#f7fcb9', '#d9f0a3', '#addd8e', '#78c679', '#41ab5d', '#238443', '#006837', '#004529']}
+          canvasWidth={800}
+          canvasHeight={800}
+        />}
       </div>
     )
   }
--- front-end/src/UserVotesPieChart.jsx
+++ front-end/src/UserVotesPieChart.jsx
@@ -4,17 +4,6 @@ import CustomTooltip from './CustomTooltip'
 
 export default class UserVotesPieChart extends Component {
     max = 0
-    COLORS = [
-        '#ffffe5',
-        '#f7fcb9',
-        '#d9f0a3',
-        '#addd8e',
-        '#78c679',
-        '#41ab5d',
-        '#238443',
-        '#006837',
-        '#004529'
-    ]
 
     buildLayer(key, data, x, y, innerRadius, outerRadius) {
         return <Pie
@@ -27,7 +16,7 @@ export default class UserVotesPieChart extends Component {
             outerRadius={outerRadius}>
             {data.map(entry => <Cell
                 key={entry.name}
-                fill={this.COLORS[this.getColor(entry.heat)]}
+                fill={this.props.colors[this.getColor(entry.heat)]}
             />)}
         </Pie>
     }
@@ -51,18 +40,22 @@ export default class UserVotesPieChart extends Component {
 
             return ({ name: key, value: 1, heat: value.length, who: value })
         })
-        
+
         let layers = []
 
-        const centerX = 400
-        const centerY = 400
-
         for (let i = 0, startRadius = 60, endRadius = 80; i < 10; i++, startRadius += 30, endRadius += 30) {
-            layers[i] = this.buildLayer(i, myData.slice(i * 100 + 1, i * 100 + 99), centerX, centerY, startRadius, endRadius)
+            layers[i] = this.buildLayer(
+                `layer${i}`,
+                myData.slice(i * 100 + 1, i * 100 + 99),
+                this.props.canvasWidth / 2,
+                this.props.canvasHeight / 2,
+                startRadius,
+                endRadius
+            )
         }
 
         return (
-            <PieChart width={800} height={800}>
+            <PieChart width={this.props.canvasWidth} height={this.props.canvasHeight}>
                 {layers}
                 <Tooltip content={<CustomTooltip />} />
             </PieChart>
